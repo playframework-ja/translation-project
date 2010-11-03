00c0e2c13f4cb467d4621c4946f9d90876f393b3
garbagetown <garbagetown@gmail.com>
2010-11-03 12:44:20 +0900

    added all version documents.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,24 +5,26 @@ import play.mvc.Controller;
 
 public class Application extends Controller {
 
-	public static void index() {
-		render();
-	}
+    public static void index() {
+        render();
+    }
 
-	public static void documentation() throws Exception {
-		Documentation.page(
-				Play.configuration.getProperty("version.latest"), "home");
-	}
+    public static void documentation(String version) throws Exception {
+        if (version == null) {
+            version = Play.configuration.getProperty("version.latest");
+        }
+        Documentation.page(version, "home");
+    }
 
-	public static void code() {
-		render();
-	}
+    public static void code() {
+        render();
+    }
 
-	public static void ecosystem() {
-		render();
-	}
+    public static void ecosystem() {
+        render();
+    }
 
-	public static void modules() {
-		render();
-	}
+    public static void modules() {
+        render();
+    }
 }
--- app/controllers/Documentation.java
+++ app/controllers/Documentation.java
@@ -13,39 +13,39 @@ import play.mvc.Controller;
 
 public class Documentation extends Controller {
 
-	public static void page(String version, String id) throws Exception {
+    public static void page(String version, String id) throws Exception {
 
-		String latest = Play.configuration.getProperty("version.latest");
+        String latest = Play.configuration.getProperty("version.latest");
 
-		File page = new File(
-				Play.applicationPath,
-				"documentation/" + version + "/manual/" + id + ".textile");
-		
-		if (!page.exists()) {
-			if (!version.equals(latest)) {
-				page(latest, id);
-			}
-			notFound(page.getPath());
-		}
-		
-		String textile = IO.readContentAsString(page);
-		String html = toHTML(textile);
-		String title = getTitle(textile);
+        File page = new File(
+                Play.applicationPath,
+                "documentation/" + version + "/manual/" + id + ".textile");
 
-		render(version, id, html, title);
-	}
+        if (!page.exists()) {
+            if (!version.equals(latest)) {
+                page(latest, id);
+            }
+            notFound(page.getPath());
+        }
 
-	static String toHTML(String textile) {
-		String html = new MarkupParser(new TextileLanguage()).parseToHtml(textile);
-		html = html.substring(html.indexOf("<body>") + 6, html.lastIndexOf("</body>"));
-		return html;
-	}
+        String textile = IO.readContentAsString(page);
+        String html = toHTML(textile);
+        String title = getTitle(textile);
 
-	static String getTitle(String textile) {
-		if (textile.length() == 0) {
-			return "";
-		}
-		return textile.split("\n")[0].substring(3).trim();
-	}
+        render(version, id, html, title);
+    }
+
+    static String toHTML(String textile) {
+        String html = new MarkupParser(new TextileLanguage()).parseToHtml(textile);
+        html = html.substring(html.indexOf("<body>") + 6, html.lastIndexOf("</body>"));
+        return html;
+    }
+
+    static String getTitle(String textile) {
+        if (textile.length() == 0) {
+            return "";
+        }
+        return textile.split("\n")[0].substring(3).trim();
+    }
 
 }
