8e6823f1c5d9c21c90c5f97f3f67080880ad9dac
Kitora, Naoki <naoki.kitora@gmail.com>
2011-08-22 20:09:26 +0900

    delete methods (reimplementing the wheel)

--- app/util/StringUtils.java
+++ app/util/StringUtils.java
@@ -5,26 +5,13 @@ public class StringUtils {
     public static String humanize(String camelCase) {
         char[] chars = camelCase.toCharArray();
         StringBuilder sb = new StringBuilder();
-        sb.append(toUpper(chars[0]));
+        sb.append(Character.toUpperCase(chars[0]));
         for (int i = 1; i < chars.length; i++) {
-            if (isUpperCase(chars[i])) {
+            if (Character.isUpperCase(chars[i])) {
                 sb.append(' ');
             }
             sb.append(chars[i]);
         }
         return sb.toString();
     }
-
-    public static boolean isLowerCase(char c) {
-        return c >= 'a' && c <= 'z' ? true : false;
-    }
-
-    public static boolean isUpperCase(char c) {
-        return c >= 'A' && c <= 'Z' ? true : false;
-    }
-
-    public static char toUpper(char c) {
-        return isLowerCase(c) ? (char) (c - 0x20) : c;
-    }
-
 }
