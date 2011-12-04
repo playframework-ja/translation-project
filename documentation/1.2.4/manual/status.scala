39935bf8ca5dac712334037049e3c90e42d2fb07
opensas <opensas@gmail.com>
2011-12-04 13:16:19 -0300

    scala scripts improvements

--- documentation/1.2.4/manual/status.scala
+++ documentation/1.2.4/manual/status.scala
@@ -28,21 +28,17 @@ def status(title: String = "status", current: Long, total: Long, format: (Long) 
   " (" + format(total - current) + " to go)"
 }
 
-def asKB(length: Long) = (length / 1000).toString + "kb"
+def asKB(length: Long) = (length / 1000) + "kb"
 
 class DocumentationFile(val file: File) {
 
-  val name: String = file.getName
-  val length: Long = file.length
-  val isTranslated: Boolean = calculateIsTranslated
+  val name = file.getName
+  val length = file.length
+  val isTranslated = (firstLine.indexOf("Esta página todavía no ha sido traducida al castellano") == -1)
 
-  override def toString = "name: " + name + ", length: " + length.toString + ", isTranslated: " + isTranslated.toString
+  override def toString = "name: " + name + ", length: " + length + ", isTranslated: " + isTranslated
 
-  private def calculateIsTranslated: Boolean = {
-    firstLine.indexOf("Esta página todavía no ha sido traducida al castellano") == -1
-  }
-
-  def firstLine: String = {
+  def firstLine = {
     new BufferedReader(new FileReader(file)).readLine
   }
 
