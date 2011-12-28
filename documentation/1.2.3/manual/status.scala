c1ffde1f52b3afc3562d30c06baa7ecd77a6416d
opensas <opensas@gmail.com>
2011-12-28 02:55:47 -0300

    updated 1.2.3 files

---
+++ documentation/1.2.3/manual/status.scala
@@ -0,0 +1,50 @@
+#!/bin/sh
+exec scala "$0" "$@"
+!#
+
+import java.io._
+
+val docs = new File(".").listFiles
+  .filter(_.getName.endsWith(".textile"))   // process only textile files
+  .map(new DocumentationFile(_))
+
+val translated = docs.filter(_.isTranslated)    // only already translated files
+
+val translatedLength = translated.map(_.length).sum
+val docsLength = docs.map(_.length).sum
+
+println( 
+  status("translated size", translatedLength, docsLength, (length) => asKB(length) ) 
+)
+
+println( 
+  status("translated files", translated.length, docs.length) 
+)
+
+def status(
+  title: String = "status", 
+  current: Long, total: Long, 
+  format: (Long) => String = (x) => x.toString): String = {
+
+  val percent = current * 100 / total
+
+  title + ": " + format(current) + "/" + format(total) + " " +
+  percent + "%" +
+  " (pending " + format(total - current) + " " +
+  (100-percent) + "%)"
+}
+
+def asKB(length: Long) = (length / 1000) + "kb"
+
+class DocumentationFile(val file: File) {
+
+  val name = file.getName
+  val length = file.length
+  val isTranslated = (firstLine.indexOf("Esta página todavía no ha sido traducida al castellano") == -1)
+
+  override def toString = "name: " + name + ", length: " + length + ", isTranslated: " + isTranslated
+
+  def firstLine = new BufferedReader(new FileReader(file)).readLine
+
+}
+
