c7abdd56df075642f2c8a721cf53b1e9d396ed72
opensas <opensas@gmail.com>
2012-01-17 01:48:21 -0300

    style changes in status.py, added some spaces

--- documentation/1.2.4/manual/status.py
+++ documentation/1.2.4/manual/status.py
@@ -9,14 +9,14 @@ translated_files = [file for file in total_files if "Esta página todavía no ha
 
 total_size = sum([os.path.getsize(file) for file in total_files]) / 1000
 translated_size = sum([os.path.getsize(file) for file in translated_files]) / 1000
-translated_percent= translated_size * 100 / total_size
+translated_percent = translated_size * 100 / total_size
 
 print "translated size: %dkb/%dkb %d%% (pending %dkb %d%%)" % \
       (translated_size, total_size, translated_percent, total_size-translated_size, 100-translated_percent)
 
-total_count=len(total_files)
-translated_count=len(translated_files)
-translated_percent= translated_count * 100 / total_count
+total_count = len(total_files)
+translated_count = len(translated_files)
+translated_percent = translated_count * 100 / total_count
 
 print "translated files: %d/%d %d%% (pending %d %d%%)" % \
       (translated_count, total_count, translated_percent, total_count-translated_count, 100-translated_percent)
