94d5babeb338901604500a8b7723ee7811e96794
garbagetown <garbagetown@gmail.com>
2014-08-02 00:03:12 +0900

    remove the docs except 2.0.8. refs #715

--- documentation/2.2.0/manual/javaGuide/main/forms/code/javaguide/forms/controllers/Application.scala
+++
@@ -1,7 +0,0 @@
-package javaguide.forms.controllers
-
-import play.api.mvc._
-
-object Application extends Controller {
-  def submit = Action(Ok)
-}
--- documentation/2.2.0/manual/javaGuide/main/forms/code/javaguide/forms/html/routes/package.scala
+++
@@ -1,5 +0,0 @@
-package javaguide.forms.html
-
-package object routes {
-  val Application = javaguide.forms.controllers.routes.Application
-}
