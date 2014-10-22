7f7b7aeea26a18a29efd6d20724809bccdf4e404
garbagetown <garbagetown@gmail.com>
2014-10-22 22:19:37 +0900

    remove sample codes. refs #1050

--- manual/javaGuide/main/forms/code/javaguide/forms/html/User.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2009-2013 Typesafe Inc. <http://www.typesafe.com>
- */
-package javaguide.forms.html;
-
-public class User {
-    public String email;
-    public String password;
-}
